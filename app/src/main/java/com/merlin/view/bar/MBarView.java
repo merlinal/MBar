d02b2650e9a64a607d23cf00ea4ab36a7fcb27bc
ncm <anlin@newchama.comn>
2017-06-07 17:24:04 +0800

    check

--- app/src/main/java/com/merlin/view/bar/MBarView.java
+++ app/src/main/java/com/merlin/view/bar/MBarView.java
@@ -1,135 +1,135 @@
 package com.merlin.view.bar;
-//
-//import android.app.Dialog;
-//import android.content.Context;
-//import android.databinding.DataBindingUtil;
-//import android.databinding.ViewDataBinding;
-//import android.support.v7.widget.LinearLayoutManager;
-//import android.util.AttributeSet;
-//import android.view.Gravity;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.view.Window;
-//import android.view.WindowManager;
-//import android.widget.RelativeLayout;
-//
-//import com.merlin.core.context.AppContext;
-//import com.merlin.core.network.NetWorkListener;
-//import com.merlin.core.network.NetWorkType;
-//import com.merlin.core.util.UiUtil;
-//import com.merlin.core.util.Util;
-//import com.merlin.core.util.ValiUtil;
-//import com.merlin.view.bar.databinding.ViewBarBinding;
-//import com.merlin.view.bar.databinding.ViewBarMenuBinding;
-//import com.merlin.view.bar.model.Bar;
-//import com.merlin.view.bar.model.Menu;
-//import com.merlin.view.recycler.AbstractRecyclerAdapter;
-//import com.merlin.view.recycler.MRecyclerView;
-//import com.merlin.view.recycler.RecyclerViewHolder;
-//
-///**
-// * Created by ncm on 2017/4/12.
-// */
-//
-//public class MBarView extends RelativeLayout implements NetWorkListener {
-//
-//    public MBarView(Context context) {
-//        super(context);
-//    }
-//
-//    public MBarView(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//    }
-//
-//    public MBarView(Context context, AttributeSet attrs, int defStyleAttr) {
-//        super(context, attrs, defStyleAttr);
-//    }
-//
-//    private ViewBarBinding binding;
-//    private Dialog moreDialog;
-//    private Bar bar;
-//
-//    @Override
-//    public void onNetWorkChanged(NetWorkType type) {
-//        bar.setTitle(type.name());
-//    }
-//
-//    @Override
-//    protected void onFinishInflate() {
-//        super.onFinishInflate();
-//        binding = DataBindingUtil.bind(this);
-//        AppContext.inst().addNetWorkListener(this);
-//    }
-//
-//    public void apply(Bar bar) {
-//        this.bar = bar;
-//        binding.setModel(bar);
-//    }
-//
-//    public Bar model() {
-//        return bar;
-//    }
-//
-//    public void showMoreTopRight() {
-//        showMore(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
-//                0, getBottom(), Gravity.TOP | Gravity.RIGHT, true, 0);
-//    }
-//
-//    public void showMoreTopMiddle() {
-//        showMore(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
-//                0, getBottom(), Gravity.TOP | Gravity.CENTER, true, 0);
-//    }
-//
-//    public void showMoreMiddle() {
-//        showMore(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
-//                0, 0, Gravity.CENTER, true, 0);
-//    }
-//
-//    public void hideMore() {
-//        if (moreDialog != null && moreDialog.isShowing()) {
-//            moreDialog.dismiss();
-//        }
-//    }
-//
-//    public void showMore(int width, int height, int x, int y, int gravity, boolean cancelable, float alpha) {
-//        if (ValiUtil.isEmpty(bar.getMoreList())) {
-//            return;
-//        }
-//        if (moreDialog == null) {
-//            moreDialog = UiUtil.showDialog(getContext(), getMenuMoreView(alpha), width, height, x, y, 0, R.style.anim_alpha_scale, gravity, cancelable);
-//        } else {
-//            Window window = moreDialog.getWindow();
-//            if (gravity != 0) {
-//                window.setGravity(gravity);  //此处可以设置dialog显示的位置
-//            }
-//            WindowManager.LayoutParams params = window.getAttributes();
-//            params.x = x;
-//            params.y = y;
-//            moreDialog.getWindow().setAttributes(params);
-//            moreDialog.show();
-//        }
-//    }
-//
-//    private View getMenuMoreView(float alpha) {
-//        MRecyclerView recyclerView = new MRecyclerView(getContext());
-////        recyclerView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-//        recyclerView.set(MRecyclerView.MODE_LIST, LinearLayoutManager.VERTICAL, 0, Util.dp2px(1), 0, 0, 0xffffffff);
-//        recyclerView.setAdapter(new AbstractRecyclerAdapter<Menu>(bar.getMoreList()) {
-//            @Override
-//            public ViewDataBinding getItemBinding(ViewGroup parent, int viewType) {
-//                return DataBindingUtil.inflate(Util.inflater(), R.layout.view_bar_menu, parent, false);
-//            }
-//
-//            @Override
-//            public void onBindViewHolder(RecyclerViewHolder holder, int position) {
-//                ViewBarMenuBinding binding = (ViewBarMenuBinding) holder.getBinding();
-//                binding.setModel(getData(position));
-//                binding.executePendingBindings();
-//            }
-//        });
-//        recyclerView.setBackgroundColor(bar.getBgColorMore());
-//        recyclerView.setAlpha(alpha <= 0 ? 0.8f : alpha);
-//        return recyclerView;
-//    }
-//
-//}
+
+import android.app.Dialog;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+import com.merlin.core.context.AppContext;
+import com.merlin.core.network.NetWorkListener;
+import com.merlin.core.network.NetWorkType;
+import com.merlin.core.util.UiUtil;
+import com.merlin.core.util.Util;
+import com.merlin.core.util.ValiUtil;
+import com.merlin.view.bar.databinding.ViewBarBinding;
+import com.merlin.view.bar.databinding.ViewBarMenuBinding;
+import com.merlin.view.bar.model.Bar;
+import com.merlin.view.bar.model.Menu;
+import com.merlin.view.recycler.AbstractRecyclerAdapter;
+import com.merlin.view.recycler.MRecyclerView;
+import com.merlin.view.recycler.RecyclerViewHolder;
+
+/**
+ * Created by ncm on 2017/4/12.
+ */
+
+public class MBarView extends RelativeLayout implements NetWorkListener {
+
+    public MBarView(Context context) {
+        super(context);
+    }
+
+    public MBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private ViewBarBinding binding;
+    private Dialog moreDialog;
+    private Bar bar;
+
+    @Override
+    public void onNetWorkChanged(NetWorkType type) {
+        bar.setTitle(type.name());
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        binding = DataBindingUtil.bind(this);
+        AppContext.inst().addNetWorkListener(this);
+    }
+
+    public void apply(Bar bar) {
+        this.bar = bar;
+        binding.setModel(bar);
+    }
+
+    public Bar model() {
+        return bar;
+    }
+
+    public void showMoreTopRight() {
+        showMore(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+                0, getBottom(), Gravity.TOP | Gravity.RIGHT, true, 0);
+    }
+
+    public void showMoreTopMiddle() {
+        showMore(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+                0, getBottom(), Gravity.TOP | Gravity.CENTER, true, 0);
+    }
+
+    public void showMoreMiddle() {
+        showMore(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+                0, 0, Gravity.CENTER, true, 0);
+    }
+
+    public void hideMore() {
+        if (moreDialog != null && moreDialog.isShowing()) {
+            moreDialog.dismiss();
+        }
+    }
+
+    public void showMore(int width, int height, int x, int y, int gravity, boolean cancelable, float alpha) {
+        if (ValiUtil.isEmpty(bar.getMoreList())) {
+            return;
+        }
+        if (moreDialog == null) {
+            moreDialog = UiUtil.showDialog(getContext(), getMenuMoreView(alpha), width, height, x, y, 0, R.style.anim_alpha_scale, gravity, cancelable);
+        } else {
+            Window window = moreDialog.getWindow();
+            if (gravity != 0) {
+                window.setGravity(gravity);  //此处可以设置dialog显示的位置
+            }
+            WindowManager.LayoutParams params = window.getAttributes();
+            params.x = x;
+            params.y = y;
+            moreDialog.getWindow().setAttributes(params);
+            moreDialog.show();
+        }
+    }
+
+    private View getMenuMoreView(float alpha) {
+        MRecyclerView recyclerView = new MRecyclerView(getContext());
+//        recyclerView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        recyclerView.set(MRecyclerView.MODE_LIST, LinearLayoutManager.VERTICAL, 0, Util.dp2px(1), 0, 0, 0xffffffff);
+        recyclerView.setAdapter(new AbstractRecyclerAdapter<Menu>(bar.getMoreList()) {
+            @Override
+            public ViewDataBinding getItemBinding(ViewGroup parent, int viewType) {
+                return DataBindingUtil.inflate(Util.inflater(), R.layout.view_bar_menu, parent, false);
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+                ViewBarMenuBinding binding = (ViewBarMenuBinding) holder.getBinding();
+                binding.setModel(getData(position));
+                binding.executePendingBindings();
+            }
+        });
+        recyclerView.setBackgroundColor(bar.getBgColorMore());
+        recyclerView.setAlpha(alpha <= 0 ? 0.8f : alpha);
+        return recyclerView;
+    }
+
+}
--- app/src/main/java/com/merlin/view/bar/model/Menu.java
+++ app/src/main/java/com/merlin/view/bar/model/Menu.java
@@ -1,175 +1,175 @@
 package com.merlin.view.bar.model;
-//
-//import com.merlin.core.model.BaseModel;
-//import com.merlin.view.bar.MenuListener;
-//
-///**
-// * Created by ncm on 2017/4/12.
-// */
-//
-//public class Menu extends BaseModel {
-//
-//    private Menu() {
-//    }
-//
-//    private long id;
-//
-//    private int type;
-//    private int bgColor;
-//
-//    private String text;
-//    private int textColor;
-//    private float textSize;
-//
-//    private String desc;
-//    private int descColor;
-//    private float descSize;
-//
-//    private int iconId;
-//
-//    private boolean isNotice;
-//    private String notice;
-//
-//    private MenuListener listener;
-//
-//
-//    public static Menu newInst() {
-//        return new Menu();
-//    }
-//
-//    public Menu set(int iconId, String text, int textColor, int textSize, String desc, int descColor, int descSize, boolean isNotice, String notice, MenuListener listener) {
-//        iconId(iconId).listener(listener)
-//                .text(text).textColor(textColor).textSize(textSize)
-//                .desc(desc).descColor(descColor).descSize(descSize)
-//                .isNotice(isNotice).notice(notice);
-//        return this;
-//    }
-//
-//    public Menu id(long id) {
-//        this.id = id;
-//        return this;
-//    }
-//
-//    public Menu text(String text) {
-//        this.text = text;
-//        return this;
-//    }
-//
-//    public Menu textColor(int textColor) {
-//        if (textColor != 0) {
-//            this.textColor = textColor;
-//        }
-//        return this;
-//    }
-//
-//    public Menu textSize(float textSize) {
-//        if (textSize != 0) {
-//            this.textSize = textSize;
-//        }
-//        return this;
-//    }
-//
-//    public Menu desc(String desc) {
-//        this.desc = desc;
-//        return this;
-//    }
-//
-//    public Menu descColor(int descColor) {
-//        if (descColor != 0) {
-//            this.descColor = descColor;
-//        }
-//        return this;
-//    }
-//
-//    public Menu descSize(float descSize) {
-//        if (descSize != 0) {
-//            this.descSize = descSize;
-//        }
-//        return this;
-//    }
-//
-//    public Menu iconId(int iconId) {
-//        if (iconId != 0) {
-//            this.iconId = iconId;
-//        }
-//        return this;
-//    }
-//
-//    public Menu isNotice(boolean isNotice) {
-//        this.isNotice = isNotice;
-//        return this;
-//    }
-//
-//    public Menu notice(String notice) {
-//        this.notice = notice;
-//        return this;
-//    }
-//
-//    public Menu type(int type) {
-//        this.type = type;
-//        return this;
-//    }
-//
-//    public Menu bgColor(int color) {
-//        this.bgColor = color;
-//        return this;
-//    }
-//
-//    public Menu listener(MenuListener listener) {
-//        this.listener = listener;
-//        return this;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public int getType() {
-//        return type;
-//    }
-//
-//    public int getBgColor() {
-//        return bgColor;
-//    }
-//
-//    public int getTextColor() {
-//        return textColor;
-//    }
-//
-//    public float getTextSize() {
-//        return textSize;
-//    }
-//
-//    public int getDescColor() {
-//        return descColor;
-//    }
-//
-//    public float getDescSize() {
-//        return descSize;
-//    }
-//
-//    public int getIconId() {
-//        return iconId;
-//    }
-//
-//    public boolean isNotice() {
-//        return isNotice;
-//    }
-//
-//    public String getNotice() {
-//        return notice;
-//    }
-//
-//    public MenuListener getListener() {
-//        return listener;
-//    }
-//
-//    public String getText() {
-//        return text;
-//    }
-//
-//    public String getDesc() {
-//        return desc;
-//    }
-//
-//}
+
+import com.merlin.core.model.BaseModel;
+import com.merlin.view.bar.MenuListener;
+
+/**
+ * Created by ncm on 2017/4/12.
+ */
+
+public class Menu extends BaseModel {
+
+    private Menu() {
+    }
+
+    private long id;
+
+    private int type;
+    private int bgColor;
+
+    private String text;
+    private int textColor;
+    private float textSize;
+
+    private String desc;
+    private int descColor;
+    private float descSize;
+
+    private int iconId;
+
+    private boolean isNotice;
+    private String notice;
+
+    private MenuListener listener;
+
+
+    public static Menu newInst() {
+        return new Menu();
+    }
+
+    public Menu set(int iconId, String text, int textColor, int textSize, String desc, int descColor, int descSize, boolean isNotice, String notice, MenuListener listener) {
+        iconId(iconId).listener(listener)
+                .text(text).textColor(textColor).textSize(textSize)
+                .desc(desc).descColor(descColor).descSize(descSize)
+                .isNotice(isNotice).notice(notice);
+        return this;
+    }
+
+    public Menu id(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Menu text(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public Menu textColor(int textColor) {
+        if (textColor != 0) {
+            this.textColor = textColor;
+        }
+        return this;
+    }
+
+    public Menu textSize(float textSize) {
+        if (textSize != 0) {
+            this.textSize = textSize;
+        }
+        return this;
+    }
+
+    public Menu desc(String desc) {
+        this.desc = desc;
+        return this;
+    }
+
+    public Menu descColor(int descColor) {
+        if (descColor != 0) {
+            this.descColor = descColor;
+        }
+        return this;
+    }
+
+    public Menu descSize(float descSize) {
+        if (descSize != 0) {
+            this.descSize = descSize;
+        }
+        return this;
+    }
+
+    public Menu iconId(int iconId) {
+        if (iconId != 0) {
+            this.iconId = iconId;
+        }
+        return this;
+    }
+
+    public Menu isNotice(boolean isNotice) {
+        this.isNotice = isNotice;
+        return this;
+    }
+
+    public Menu notice(String notice) {
+        this.notice = notice;
+        return this;
+    }
+
+    public Menu type(int type) {
+        this.type = type;
+        return this;
+    }
+
+    public Menu bgColor(int color) {
+        this.bgColor = color;
+        return this;
+    }
+
+    public Menu listener(MenuListener listener) {
+        this.listener = listener;
+        return this;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getBgColor() {
+        return bgColor;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public int getDescColor() {
+        return descColor;
+    }
+
+    public float getDescSize() {
+        return descSize;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public boolean isNotice() {
+        return isNotice;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public MenuListener getListener() {
+        return listener;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+}
